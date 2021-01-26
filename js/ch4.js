bd2d180dda63b4d95ccbab2f9fae35a832c3765b
Rewbee <74418145+Rewbee@users.noreply.github.com>
2021-01-26 13:42:43 -0500

    Update ch4.js

--- js/ch4.js
+++ js/ch4.js
@@ -1,15 +1,8 @@
-var table = 25; // Unit of table
+var table = 50; // Unit of table
 var operator = 'addition'; // Type of calculation
 var i = 1; // Set counter to 1
 var msg = ''; // Message
-var rng = Math.floor((Math.random() * 10) + 1);
 var camvasNames = ['Filler', 'Extra Small', 'Small', 'Medium', 'Normal', 'Large', 'Extra Large', 'Wall Mural'];
-
-if (rng % 2 === 0) {
-	var operator = 'notaddition';
-} else {
-	var operator = 'addition';
-}
 
 if (operator === 'addition') {
 	// Do addition
