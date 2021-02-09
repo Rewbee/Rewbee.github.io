8cd8831bdfb0fad4e0b469bd36188c0ea24afdb7
Rewbee <74418145+Rewbee@users.noreply.github.com>
2021-02-09 12:38:23 -0500

    Delete button.js

--- js/button.js
+++
@@ -1,7 +0,0 @@
-
-var button = document.createElement("button");
-button.innerHTML = "Shop";
-
-// 2. Append somewhere
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
