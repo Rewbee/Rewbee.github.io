8c6127c0751de9f5a7c9200ae04c52c85ba525fa
Rewbee <74418145+Rewbee@users.noreply.github.com>
2021-03-10 11:19:50 -0500

    Add files via upload

---
+++ js/angular-controller.js
@@ -0,0 +1,22 @@
+function Small ($scope) {
+  $scope.description = 'Small Canvas';
+  $scope.cost = 15;
+  $scope.qty = 1;
+}
+
+function Normal ($scope) {
+  $scope.description = 'Normal Canvas';
+  $scope.cost = 25;
+  $scope.qty = 1;
+}
+
+function Medium ($scope) {
+  $scope.description = 'Medium Canvas';
+  $scope.cost = 30;
+  $scope.qty = 1;
+}
+function Large ($scope) {
+  $scope.description = 'Large Canvas';
+  $scope.cost = 45;
+  $scope.qty = 1;
+}
